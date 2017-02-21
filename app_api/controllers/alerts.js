3045b7dda9bff8fd89b69371f0fe57c301230d1d
Joni <joni.suhonen@metropolia.fi>
2017-02-21 21:07:09 +0200

    Added alert API structure

---
+++ app_api/controllers/alerts.js
@@ -0,0 +1,32 @@
+//MONGOOSE
+var mongoose = require('mongoose');
+
+var Alerts = mongoose.model('Alerts');
+
+var sendJSONresponse = function(res, status, content) {
+  res.status(status);
+  res.json(content);
+};
+
+	//Get Alerts
+	module.exports.getAlerts = function(req, res) {
+		 Alerts.find(function (err, alerts) {
+	  		if(err) {
+	            sendJSONresponse(res, 404, err);
+	        }else{
+	            sendJSONresponse(res, 200, alerts);
+	        }
+		});
+		
+	};
+
+	//Post all alerts
+	module.exports.postAlerts = function(req, res) {
+		
+		
+	};
+	//Post one alert
+	module.exports.postNewAlert = function(req, res) {
+		
+		
+	};
